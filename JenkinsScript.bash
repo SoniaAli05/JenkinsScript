8d57fc1045adcea757356df2e0939d8f11f8a443
Sonia Ali <soniaali005@gmail.com>
2019-01-09 11:46:53 +0000

    Jenkins Script

---
+++ JenkinsScript.bash
@@ -0,0 +1,5 @@
+pwd
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install -y jenkins
+sudo yum install -y java
